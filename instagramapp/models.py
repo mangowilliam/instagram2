09567f83a9d4ea0ec36fbf03bc7d79420ee1c6d0
williammango <williammango2015@gmail.com>
2019-07-26 16:29:30 +0300

    created models,migration and migrated them

--- instagramapp/models.py
+++ instagramapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    photo = models.ImageField(upload_to='images/')
+    bio = models.CharField(max_length=150)
+    
+    
+    def __str__(self):
+        return self.bio
+    
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=60)
+    caption = models.CharField(max_length=150)
+    profile = models.ForeignKey(Profile)
+    likes = models.IntegerField()
+    comments = models.CharField(max_length=200)
+    
+    
+    def __str__(self):
+        return self.name
