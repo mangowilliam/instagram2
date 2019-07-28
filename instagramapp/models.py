68a4e472a55679e7e2abfe3a749a4c2c1261c159
williammango <williammango2015@gmail.com>
2019-07-28 12:57:18 +0300

    update database

--- instagramapp/models.py
+++ instagramapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 class Profile(models.Model):
@@ -13,7 +15,9 @@ class Image(models.Model):
     image = models.ImageField(upload_to='images/')
     name = models.CharField(max_length=60)
     caption = models.CharField(max_length=150)
-    profile = models.ForeignKey(Profile)
+    profile = models.ForeignKey(Profile) 
+    writer = models.ForeignKey(User, on_delete = models.CASCADE)
+    ondate = models.DateTimeField(default= timezone.now)
     likes = models.IntegerField()
     comments = models.CharField(max_length=200)
     
--- instagramapp/views.py
+++ instagramapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from.models import Image
+from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
+
+
 
 # Create your views here.
 def instagram(request):
